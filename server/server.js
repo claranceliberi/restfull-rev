0837865a18ecf300fea431f80b0facbb3c94001d
claranceliberi <liberintwari@gmail.com>
2022-07-06 05:45:28 +0200

    chore: node_modules  volume

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 async function startApp(app){
     try{
         await mongoose.connect(
-            "mongodb://root:root@mongo:27017/mongoose?authSource=admin"
+            "mongodb://mongo:27017/voting"
         );
         app.listen(3000,() => console.log('App started on port 3000'))
     }catch(error){
